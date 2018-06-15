d24e329b94c136629da7ae2e1265a698e3f3ff55
Kevin McAnoy <kmcanoy@redhat.com>
2018-06-15 10:22:30 -0700

    update image promotion

--- vars/promoteImageWithinCluster.groovy
+++ vars/promoteImageWithinCluster.groovy
@@ -1,14 +1,14 @@
-def call( String appName, String targetImageName, String sourceProjectName, String targetProjectName, String deployTag){
+def call(String sourceImage, String targetImageName, String deployImageTag){
     openshift.withCluster () {
         try {
-            echo "Tagging ${targetProjectName}/${targetImageName}:latest ${targetProjectName}/${targetImageName}:previous-${deployTag}"
-            openshift.tag( "${targetProjectName}/${targetImageName}:latest ${targetProjectName}/${targetImageName}:previous-${deployTag}" )
+            echo "Tagging ${targetImageName}:${deployImageTag} ${targetImageName}:${deployImageTag}-previous"
+            openshift.tag( "${targetImageName}:{$deployImageTag} ${targetImageName}:${deployImageTag}-previous" )
         } catch (Exception ex) {
             echo 'Failed to tag for previous'
         }
-        echo "Tagging ${sourceProjectName}/${appName}:latest ${targetProjectName}/${targetImageName}:latest-${deployTag}"
-        openshift.tag( "${sourceProjectName}/${appName}:latest ${targetProjectName}/${targetImageName}:latest-${deployTag}" )
-        echo "Tagging ${sourceProjectName}/${appName}:latest ${targetProjectName}/${targetImageName}:${deployTag}"
-        openshift.tag( "${sourceProjectName}/${appName}:latest ${targetProjectName}/${targetImageName}:${deployTag}" )
+        echo "Tagging ${sourceImage} ${targetProjectName}/${targetImageName}:${deployImageTag}-latest"
+        openshift.tag( "${sourceImage} ${targetProjectName}/${targetImageName}:${deployImageTag}-latest" )
+        echo "Tagging ${sourceImage} ${targetProjectName}/${targetImageName}:${deployImageTag}"
+        openshift.tag( "${sourceImage} ${targetProjectName}/${targetImageName}:${deployImageTag}" )
     }
 }
