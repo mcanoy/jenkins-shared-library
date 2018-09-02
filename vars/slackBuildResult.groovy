e1ae9a007cdea2435a655a05bb47c7a4d6ea2385
Kevin McAnoy <kmcanoy@redhat.com>
2018-09-01 17:01:10 -0700

    update build result

--- vars/slackBuildResult.groovy
+++ vars/slackBuildResult.groovy
@@ -1,14 +1,14 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: ":success: Job: ${env.JOB_NAME} ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: ":success: Job: ${env.JOB_NAME} <${env.BUILD_URL}|${env.BUILD_NUMBER}> was successful"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: ":epic_fail: Job: ${env.JOB_NAME} ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: ":epic_fail: Job: ${env.JOB_NAME} <${env.BUILD_URL}|${env.BUILD_NUMBER}> was failed"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: ":fry: Job: ${env.JOB_NAME} ${env.BUILD_NUMBER} was unstable"
+    slackSend color: "warning", message: ":fry: Job: ${env.JOB_NAME} <${env.BUILD_URL}|${env.BUILD_NUMBER}> was unstable"
   }
   else {
-    slackSend color: "danger", message: ":fry: Job: ${env.JOB_NAME} ${env.BUILD_NUMBER} its result was unclear"	
+    slackSend color: "danger", message: ":fry: Job: ${env.JOB_NAME} <${env.BUILD_URL}|${env.BUILD_NUMBER}> its result was unclear"
   }
 }
